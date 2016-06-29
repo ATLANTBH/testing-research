bd06b33dbcba876293373ce2f1b9827266d878c0
bakir <bakir@atlantbh.com>
2016-06-29 15:05:13 +0200

    Modified vagrantfile, added provision script

--- parallel_mobile_execution/ios/scripts/update_udid.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-system_profiler_path=`which system_profiler`
-
-UDID=`$system_profiler_path SPUSBDataType | grep "Serial Number: " | awk '{ print $3 }'`
-
-sed -i.bak "s/udid:.*$/udid: \"$UDID\"/" spec/spec_helper.rb
